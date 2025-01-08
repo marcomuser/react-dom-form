31c4e19068338e8862753d8600b6a3a37926c580
Marco Muser <marco.muser@posteo.de>
2025-01-08 07:37:01 +0100

    use for loop instead of reduce

--- packages/core/src/parse.ts
+++ packages/core/src/parse.ts
@@ -4,7 +4,9 @@ import type { StructuredFormValue } from "./types.js";
 export function parse<
   FormValues extends Record<PropertyKey, any> = Record<PropertyKey, unknown>,
 >(formData: FormData): StructuredFormValue<FormValues> {
-  return formData.entries().reduce((acc, [key, val]) => {
-    return set(acc, key, val);
-  }, {} as StructuredFormValue<FormValues>);
+  let formValues = {} as StructuredFormValue<FormValues>;
+  for (const [key, val] of formData.entries()) {
+    formValues = set(formValues, key, val);
+  }
+  return formValues;
 }
