49d4e92cddddb34778056a8767635905b3916122
Marco Muser <marco.muser@posteo.de>
2025-01-16 21:32:14 +0100

    combine FormProviderProps

--- packages/core/src/FormProvider.tsx
+++ packages/core/src/FormProvider.tsx
@@ -3,19 +3,13 @@ import type { UnknownRecord } from "type-fest";
 import { getFieldProps } from "./getFieldProps.js";
 import { FormContext, type FormContextValue } from "./FormContext.js";
 
-export interface FormProviderProps<
+interface FormProviderProps<
   DefaultValues extends UnknownRecord | undefined,
   SubmitError extends UnknownRecord | undefined,
 > {
   defaultValues?: DefaultValues;
   submitError?: SubmitError;
   ref: RefObject<HTMLFormElement | null>;
-}
-
-interface ChildrenProp<
-  DefaultValues extends UnknownRecord | undefined,
-  SubmitError extends UnknownRecord | undefined,
-> {
   children:
     | ReactNode
     | ((props: FormContextValue<DefaultValues, SubmitError>) => ReactNode);
@@ -29,8 +23,7 @@ export function FormProvider<
   submitError,
   ref,
   children,
-}: FormProviderProps<DefaultValues, SubmitError> &
-  ChildrenProp<DefaultValues, SubmitError>): JSX.Element {
+}: FormProviderProps<DefaultValues, SubmitError>): JSX.Element {
   return (
     <FormContext
       value={{
