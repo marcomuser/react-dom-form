641e8a83496dc9851fb54cd007f94eb2bb3ae8f0
Marco Muser <marco.muser@posteo.de>
2025-01-16 21:28:34 +0100

    move jsdoc to context

--- packages/core/src/FormProvider.tsx
+++ packages/core/src/FormProvider.tsx
@@ -7,62 +7,8 @@ export interface FormProviderProps<
   DefaultValues extends UnknownRecord | undefined,
   SubmitError extends UnknownRecord | undefined,
 > {
-  /**
-   * Default values for the form. This is typically used in conjunction with
-   * `useActionState` to display initial values.
-   *
-   * @example
-   * ```tsx
-   * const [formState, submitAction] = useActionState(
-   *   async (prevState, formData) => {
-   *     const formValues = parse(formData);
-   *     // ... form submission logic
-   *     return redirect("/success")
-   *   },
-   *   { defaultValues, submitError: undefined }
-   * );
-   *
-   * return <FormProvider defaultValues={formState.defaultValues}>...</FormProvider>;
-   * ```
-   */
   defaultValues?: DefaultValues;
-  /**
-   * Error object returned from a form submission. This is meant to be used with
-   * `useActionState` to display submit errors to the user.
-   *
-   * @example
-   * ```tsx
-   * const [formState, submitAction] = useActionState(
-   *   async (prevState, formData) => {
-   *     const formValues = parse(formData);
-   *     try {
-   *       // ... form submission logic
-   *     } catch (error) {
-   *       return { defaultValues: formValues, submitError: error.message };
-   *     }
-   *   },
-   *   { defaultValues, submitError: undefined }
-   * );
-   *
-   * return <FormProvider submitError={formState.submitError}>...</FormProvider>;
-   * ```
-   */
   submitError?: SubmitError;
-  /**
-   * A ref to the HTML form element. This is required for accessing the form
-   * element directly, for example, to trigger form validation.
-   * This ref should be created using `useRef` in the parent component.
-   *
-   * @example
-   * ```tsx
-   * const formRef = useRef<HTMLFormElement>(null);
-   * <form ref={formRef}>
-   *   <FormProvider ref={formRef}>
-   *     ...
-   *   </FormProvider>
-   * </form>
-   * ```
-   */
   ref: RefObject<HTMLFormElement | null>;
 }
 
