8a82ef5b84a787e4804fee6fd604aa290498a78e
Marco Muser <marco.muser@posteo.de>
2025-01-17 10:50:35 +0100

    fix SerializedValue

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -14,15 +14,17 @@ export type ParsedValue<Value> = Value extends UnknownRecord
             ? undefined
             : Value;
 
-export type SerializedValue<Value> = Value extends UnknownRecord
-  ? { [Key in keyof Value]: SerializedValue<Value[Key]> }
-  : Value extends Array<infer Item>
-    ? Array<SerializedValue<Item>>
-    : Value extends number | Date | bigint
-      ? string
-      : Value extends null
-        ? undefined
-        : Value;
+export type SerializedValue<Value> = Value extends Blob | FileList
+  ? Value
+  : Value extends Date | number | bigint | string
+    ? string
+    : Value extends null
+      ? undefined
+      : Value extends Array<infer Item>
+        ? Array<SerializedValue<Item>>
+        : Value extends AnyRecord
+          ? { [Key in keyof Value]: SerializedValue<Value[Key]> }
+          : Value;
 
 export type PathsFromObject<BaseType> = Paths<
   BaseType,
