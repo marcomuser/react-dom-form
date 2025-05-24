06ae3b2fae37e33db74f5bfda267ebc0421a8036
Marco Muser <marco.muser@posteo.de>
2025-05-24 11:14:08 +0200

    extend vitest workspace config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,24 +2,27 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    browser: {
+      enabled: true,
+      provider: "playwright",
+      instances: [
+        {
+          browser: "chromium",
+        },
+      ],
+    },
+    typecheck: {
+      enabled: true,
+      tsconfig: "./tsconfig.json",
+    },
     workspace: [
       {
+        extends: true,
         test: {
           name: "core",
           include: ["packages/core/test/**/*.test.{ts,tsx}"],
-          browser: {
-            enabled: true,
-            provider: "playwright",
-            instances: [
-              {
-                browser: "chromium",
-              },
-            ],
-          },
           typecheck: {
-            enabled: true,
             include: ["packages/core/test/**/*.test-d.{ts,tsx}"],
-            tsconfig: "./tsconfig.json",
           },
         },
       },
